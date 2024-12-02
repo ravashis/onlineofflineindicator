d57a401128849b0c478f99584f1add36884da343
Rakshit P R Vashishta <rakshitprvashishta@Rakshits-MacBook-Pro.local>
2024-12-02 19:01:36 +0530

    Initialize new repo

---
+++ src/main/java/com/rvpocs/onlineofflineindicator/OnlineofflineindicatorApplication.java
@@ -0,0 +1,13 @@
+package com.rvpocs.onlineofflineindicator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineofflineindicatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineofflineindicatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rvpocs/onlineofflineindicator/OnlineofflineindicatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rvpocs.onlineofflineindicator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineofflineindicatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
